67f70764b2bc66e234b4f06cdd12ba336fb94584
notsoseamless <john.oldman@polehill.co.uk>
2017-12-04 13:09:01 +0000

    development

--- mancala.py
+++ mancala.py
@@ -12,18 +12,18 @@ import subprocess
 # constants
 
 
-def timing_function_decorator(timed_function):
+def timing_function(timed_function):
     ''' Outputs time a function takes to execute '''
     def wrapper():
         ''' wrapper '''
         t_start = time.time()
         timed_function()
         t_end = time.time()
-        return "Script ran for " + str((t_end - t_start)) + " seconds\n"
+        return 'Script ran for {} seconds\n'.format(str((t_end - t_start)))
     return wrapper
 
 
-@timing_function_decorator
+@timing_function
 def main():
     ''' main function '''
     m = Mancala('M')
@@ -31,7 +31,8 @@ def main():
 
 
 
-
+# http://www.pressmantoy.com/game-rules/Mancala_rules.pdf
+# http://www.ultraboardgames.com/mancala/game-rules.php
 
 class Mancala(object):
     ''' mancala container '''
@@ -43,23 +44,24 @@ class Mancala(object):
     def dump_status(self):
         ''' outputs text status of the game '''
         print 'Mancala Status\n'
-        print ('Store A     1    2    3    4    5    6      Store B\n'
+        print ('Store B     B6   B5   B4   B3   B2   B1      Store A\n'
                '   {}        {}    {}    {}    {}    {}    {}        {}\n'
-               '            {}    {}    {}    {}    {}    {}\n\n'.format(
-                self._player_A_store, 
-                self._pits[0], 
-                self._pits[1], 
-                self._pits[2], 
-                self._pits[3], 
-                self._pits[4], 
-                self._pits[5], 
-                self._player_B_store,
-                self._pits[6],
-                self._pits[7],
-                self._pits[8],
-                self._pits[9],
-                self._pits[10],
-                self._pits[11]))
+               '            {}    {}    {}    {}    {}    {}\n'
+               '            A1   A2   A3   A4   A5   A6\n\n'.format(
+                self._player_B_store, 
+                self._pits[11], 
+                self._pits[10], 
+                self._pits[9], 
+                self._pits[8], 
+                self._pits[7], 
+                self._pits[6], 
+                self._player_A_store,
+                self._pits[0],
+                self._pits[1],
+                self._pits[2],
+                self._pits[3],
+                self._pits[4],
+                self._pits[5]))
 
 
 
