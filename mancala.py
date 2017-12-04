2a5b43039d66c60594699f50a17d59250de7728b
notsoseamless <john.oldman@polehill.co.uk>
2017-12-04 10:28:15 +0000

    development

--- mancala.py
+++ mancala.py
@@ -27,6 +27,7 @@ def timing_function_decorator(timed_function):
 def main():
     ''' main function '''
     m = Mancala('M')
+    m.dump_status()
 
 
 
@@ -36,9 +37,29 @@ class Mancala(object):
     ''' mancala container '''
     def __init__(self, name):
         ''' constructor '''
-        print 'created' 
-
-
+        self._pits = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+        self._player_A_store = 0
+        self._player_B_store = 0
+    def dump_status(self):
+        ''' outputs text status of the game '''
+        print 'Mancala Status\n'
+        print ('Store A     1    2    3    4    5    6      Store B\n'
+               '   {}        {}    {}    {}    {}    {}    {}        {}\n'
+               '            {}    {}    {}    {}    {}    {}\n\n'.format(
+                self._player_A_store, 
+                self._pits[0], 
+                self._pits[1], 
+                self._pits[2], 
+                self._pits[3], 
+                self._pits[4], 
+                self._pits[5], 
+                self._player_B_store,
+                self._pits[6],
+                self._pits[7],
+                self._pits[8],
+                self._pits[9],
+                self._pits[10],
+                self._pits[11]))
 
 
 
